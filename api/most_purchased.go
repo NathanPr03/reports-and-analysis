fb206ea75d91b48f97d024687dfbda5fe123e8e0
Nathan03nable <nathan.pringle@n-able.com>
2024-08-04 16:06:23 +0100

    add headers

--- api/most_purchased.go
+++ api/most_purchased.go
@@ -40,6 +40,9 @@ func MostPurchased(w http.ResponseWriter, r *http.Request) {
 
 	response := map[string][]PurchasedProduct{"products": purchasedProducts}
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
 	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(response)
 }
